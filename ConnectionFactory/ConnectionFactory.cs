06028964d63426c68c29e586b44d1c673a3f4eea
nico <nico.a.judge@gmail.com>
2023-04-23 13:38:25 -0400

    added more logic and did some refactoring

--- ConnectionFactory/ConnectionFactory.cs
+++ ConnectionFactory/ConnectionFactory.cs
@@ -1,14 +1,52 @@
-﻿namespace ConnectionFactory;
+﻿
+using System.Data;
+using System.Data.SQLite;
+using Microsoft.Data.Sqlite;
+
+namespace ConnectionFactory;
+
+public class ConnectionOptions
+{
+    public string DBConnectionPath { get; set; }
+    public SqliteOpenMode SqliteOpenMode { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string DataSource { get; set; }
+}
 
 public static class ConnectionFactory
 {
-    public static void CreateSqliteInstance()
+    public static void CreateSqliteInstance(string filePath)
     {
-        
+        if (File.Exists(filePath)) return;
+        SQLiteConnection.CreateFile(filePath);
     }
 
-    public static void DeleteSqliteInstance()
+    public static SqliteConnection CreateConnection(ConnectionOptions connectionOptions)
     {
-        throw new NotImplementedException();
+        var connectionStringBuilder = new SQLiteConnectionStringBuilder
+        {
+            SyncMode = SynchronizationModes.Normal,
+            Pooling = true,
+            DataSource = connectionOptions.DataSource,
+            DefaultTimeout = 30,
+            ReadOnly = false,
+            Password = connectionOptions.Password,
+            DateTimeFormat = SQLiteDateFormats.ISO8601,
+            DateTimeKind = DateTimeKind.Utc,
+            BaseSchemaName = null,
+            JournalMode = SQLiteJournalModeEnum.Delete,  //turn this off for increased performance
+            DefaultIsolationLevel = (IsolationLevel)0,
+            DefaultDbType = DbType.AnsiString,
+            Flags = SQLiteConnectionFlags.None,
+        };
+
+        return new SqliteConnection(connectionStringBuilder.ConnectionString);
+    }
+
+    public static void DeleteSqliteInstance(string filePath)
+    {
+        if (!File.Exists(filePath)) return;
+        File.Delete(filePath);
     }
 }
--- SqliteExtensions.UnitTests/ConnectionFactory.cs
+++ SqliteExtensions.UnitTests/ConnectionFactory.cs
@@ -3,13 +3,23 @@ namespace SqliteExtensions.UnitTests;
 [TestClass]
 public class ConnectionFactoryTests
 {
-    [TestMethod]
+    [TestMethod,Priority(1)]
     public void CreateSqliteDatabase()
     {
+        ConnectionFactory.ConnectionFactory.CreateSqliteInstance("factoryDBCreationAndTearDownTests.sqlite");
+        Assert.IsTrue(File.Exists("factoryDBCreationAndTearDownTests.sqlite"));
+    }
+    
+    [TestMethod,Priority(2)]
+    public void DeleteSqliteDatabase()
+    {
+        ConnectionFactory.ConnectionFactory.DeleteSqliteInstance("factoryDBCreationAndTearDownTests.sqlite");
+        Assert.IsTrue(!File.Exists("factoryDBCreationAndTearDownTests.sqlite"));
     }
     
     [TestMethod]
-    public void DeleteSqliteDatabase()
+    public void CreateDbAndCreateConnection()
     {
+        
     }
 }
